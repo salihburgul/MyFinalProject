4e8da45febfb65d71d676c0c671bebf883ed9178
Salih Burgul <salihburgul98@gmail.com>
2021-12-17 18:15:16 +0300

    project

---
+++ Bussines/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussines.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{// IEntity implement eden lass bir veri tabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
